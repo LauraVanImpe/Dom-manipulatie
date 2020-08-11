21362bcc40311264b3116519665a1ba0ba081511
LaVi <49454999+LauraVanImpe@users.noreply.github.com>
2020-08-11 13:06:47 +0200

    Created first files

---
+++ scripts.js
@@ -0,0 +1,13 @@
+/* 
+Maak een array met vijf kleuren in het engels erin.
+Zorg ervoor dat de tekst van het <h1> element naar een willekeurige kleur verandert zodra je op de knop klikt.
+*/
+
+const Name = "Laura";
+
+function greetingFunction() {
+    document.getElementById("name").innerHTML = "Hi " + Name + "!";
+  };
+
+var colours = ["blue", "green", "yellow", "orange", "purple"];
+
